7f1be05ff0df5b90d9d5df6367ffa09ca09d1fb8
Lauren Boland <laurenmichaelab@gmail.com>
2021-10-14 17:51:26 -0400

    updating pair work

--- 4 - Updates/constraints-and-transactions-exercises-pair.sql
+++ 4 - Updates/constraints-and-transactions-exercises-pair.sql
@@ -56,19 +56,79 @@ ROLLBACK TRANSACTION
 -- The conflict occurred in database "World", table "dbo.country", column 'capital'.
 
 -- 6. Return the US capital to Washington.
+SELECT * FROM city WHERE name = 'Washington'
+
+UPDATE
+	country
+SET
+	capital = 3813
+WHERE
+	code = 'USA'
+
+SELECT * FROM country WHERE code = 'USA';
 
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+
+BEGIN TRANSACTION
+
+DELETE FROM
+city
+WHERE 
+name = 'Smallville'
+
+COMMIT TRANSACTION
+
+SELECT *
+FROM city
+WHERE district='Kansas'
 
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972
 -- (exclusive).
 -- (590 rows affected)
 
+BEGIN TRANSACTION
+
+UPDATE
+cola
+SET
+isofficial = 1 - isofficial
+FROM
+countrylanguage cola
+INNER JOIN country cou ON cou.code = cola.countrycode
+WHERE
+indepyear > 1800 AND indepyear < 1972
+
+ROLLBACK TRANSACTION
+
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round up to
 -- the nearest integer value.)
 -- (4079 rows affected)
+
+BEGIN TRANSACTION
+
+UPDATE
+	city
+SET
+	population = CEILING(population / 1000)
+
+ROLLBACK TRANSACTION
 
 -- 10. Assuming a country's surfacearea is expressed in square miles, convert it to
 -- square meters for all countries where French is spoken by more than 20% of the
 -- population.
 -- (7 rows affected)
+
+BEGIN TRANSACTION
+
+UPDATE
+cou
+SET
+cou.surfacearea = (cou.surfacearea * 2560000)
+FROM
+countrylanguage cola
+INNER JOIN country cou ON cou.code = cola.countrycode
+WHERE
+cola.language = 'French' AND cola.percentage > 20
+
+ROLLBACK TRANSACTION
