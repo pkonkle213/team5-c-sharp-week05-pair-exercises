7958e9862cd1f746418649509237e1e653c8540e
Phillip Konkle <pkonkle213@gmail.com>
2021-10-14 17:05:47 -0400

    Part 1 of Section 2 of module 2 for week 5 of class 15

--- 4 - Updates/constraints-and-transactions-exercises-pair.sql
+++ 4 - Updates/constraints-and-transactions-exercises-pair.sql
@@ -4,15 +4,56 @@
 -- 1. Add Superman's hometown, Smallville, Kansas to the city table. The
 -- countrycode is 'USA', and population of 45001.
 
+INSERT INTO city (name, countrycode, district, population)
+VALUES ('Smallville','USA','Kansas',45001)
+
+SELECT *
+FROM city
+WHERE district='Kansas'
+
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
+
+INSERT INTO countrylanguage (countrycode,language,isofficial,percentage)
+VALUES ('USA','Kryptonese',0,0.0001)
+
+SELECT *
+FROM countrylanguage
+WHERE language='Kryptonese'
 
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble." Change
 -- the appropriate record accordingly.
 
--- 4. Set the US capital to Smallville, Kansas in the country table.
+UPDATE countrylanguage
+SET language = 'Krypto-babble'
+WHERE language='Kryptonese'
+
+SELECT *
+FROM countrylanguage
+WHERE language='Krypto-babble'
+
+
+-- 4. Set the US capital to Smallville, Kansas in the country table. 4080
+
+UPDATE country
+SET capital = 4080
+WHERE code = 'USA'
+
+SELECT *
+FROM country
+WHERE code = 'USA'
 
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+
+BEGIN TRANSACTION
+
+DELETE FROM city
+WHERE name = 'Smallville'
+
+ROLLBACK TRANSACTION
+
+-- This does not work due to Smallville being the capital of USA. With the Country sheet having a FK of capital, Smallville would have to not be the capital
+-- The conflict occurred in database "World", table "dbo.country", column 'capital'.
 
 -- 6. Return the US capital to Washington.
 
