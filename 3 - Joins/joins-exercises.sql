340f6987ad13fb3156f037231c65d4164c274051
Phillip Konkle <pkonkle213@gmail.com>
2021-10-14 14:06:15 -0400

    Partner work part 1

--- 3 - Joins/joins-exercises.sql
+++ 3 - Joins/joins-exercises.sql
@@ -4,17 +4,70 @@
 -- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
 -- (36 rows)
 
+SELECT
+	cit.name AS CityName,
+	cty.name AS CountryName,
+	cit.population
+FROM
+	country cty
+	INNER JOIN city cit ON cit.countrycode = cty.code
+WHERE
+	cty.continent = 'Europe' AND cit.population > 1000000
+
 -- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
 -- (2 rows)
+
+SELECT
+	cit.name AS CityName,
+	cty.name AS CountryName,
+	cit.population
+FROM
+	country cty
+	INNER JOIN city cit ON cit.countrycode = cty.code
+	INNER JOIN countrylanguage cl ON cl.countrycode = cty.code
+WHERE
+	cit.population > 1000000 AND cl.language = 'French' AND cl.isofficial = 1
 
 -- 3. The name of the countries and continents where the language Javanese is spoken
 -- (1 row)
 
+SELECT
+	c.name,
+	c.continent,
+	cl.language
+FROM
+	country c
+	INNER JOIN countrylanguage cl ON cl.countrycode = c.code
+WHERE
+	cl.language = 'Javanese'
+
+
 -- 4. The names of all of the countries in Africa that speak French as an official language
 -- (5 row)
 
+SELECT
+	c.name,
+	c.continent,
+	cl.language,
+	cl.isofficial
+FROM
+	country c
+	INNER JOIN countrylanguage cl ON cl.countrycode = c.code
+WHERE
+	cl.language = 'French' AND cl.isofficial = 1 AND c.continent = 'Africa'
+
+
 -- 5. The average city population of cities in Europe
 -- (average city population in Europe: 287,684)
+
+SELECT
+	AVG(cit.population) AS AveragePopulation
+FROM
+	country cou
+	INNER JOIN city cit ON cit.countrycode = cou.code
+WHERE
+	cou.continent='Europe'
+
 
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
